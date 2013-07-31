1482363c79af974422909df917dcca02ff0b47ef
Steve Kenworthy <steveyken@gmail.com>
2013-07-31 16:04:04 +0800

    Fixed 'access denied' issue by passing file in over pipe.

--- lib/littleneck_clamav/clamd.rb
+++ lib/littleneck_clamav/clamd.rb
@@ -8,6 +8,14 @@ class LittleneckClamAV
       "clamdscan"
     end
 
+    def scan(path)
+      check_scan! path
+      opts = { :swallow_stderr => true, :expected_outcodes => [0, 1] }
+      params = "--no-summary -<#{path}"
+      output = Cocaine::CommandLine.new( command, params, opts ).run
+      parse_result path, output, $?.exitstatus
+    end
+
   end
 
 end
